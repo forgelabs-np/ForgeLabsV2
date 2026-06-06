26021ac750fafe5de6db52b20c8aa5216353696a
Ashish Thapa <tryaac1ss@gmail.com>
2026-06-06 16:04:20 +0000

    push to main branch

---
+++ dist/static/chunks/src_app_page_tsx_d7e36a03._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_8793c519._.js",
+  "static/chunks/node_modules_next_d6ca65f3._.js",
+  "static/chunks/node_modules_framer-motion_dist_es_e56254b9._.js",
+  "static/chunks/node_modules_react-icons_si_index_mjs_86623806._.js",
+  "static/chunks/node_modules_react-icons_lib_844c6c50._.js",
+  "static/chunks/node_modules_9efc5cf6._.js"
+],
+    source: "dynamic"
+});
